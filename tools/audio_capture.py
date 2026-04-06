9113342e610c3c32e94b9aa48220edcdd46e2708
Quentin Anciaux <quentin.anciaux@allcolor.org>
2026-04-06 10:10:24 +0200

    fix: restore parec latency to 20ms (stable with wallclock pacing)

--- tools/audio_capture.py
+++ tools/audio_capture.py
@@ -155,7 +155,7 @@ def _capture_loop(source: str):
 
             proc = subprocess.Popen(
                 ["parec", "--format=s16le", "--rate=48000", "--channels=1",
-                 "-d", monitor, "--latency-msec=80"],
+                 "-d", monitor, "--latency-msec=20"],
                 stdout=subprocess.PIPE, stderr=subprocess.DEVNULL,
                 bufsize=frame_bytes)
 
